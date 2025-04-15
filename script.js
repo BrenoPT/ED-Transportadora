5d638bd7da1a0301d07846642a4685f2d356a4ff
willasbrum <[willasmbrum@gmail.com]>
2025-04-15 16:31:29 -0300

    adicionei mais animacoes na entrega

--- script.js
+++ script.js
@@ -20,6 +20,67 @@ async function adicionarPacote() {
     fila.push(pacote);
     atualizarFila();
 
+async function entregarPacote() {
+    botao = document.getElementById('entregar-pacote')
+    botao.disabled = true
+    botao.classList.add('ativado')
+    if((fila.length) > 0){
+        await simulaEntrega()
+        filaEntrega.push(fila[0])
+        fila.shift()
+        atualizarHistorico()
+        atualizarFila();
+    }
+    botao.classList.remove('ativado')
+    botao.disabled = false
+}
+
+async function simulaEntrega() {
+    // Mostrar as informações de origem, tempo e destino
+    const infoEntrega = document.getElementById("info-entrega");
+    infoEntrega.style.display = "flex"; // Mostrar as informações durante a entrega
+
+    document.getElementById("origem-entrega").textContent = "Sede da Transportadora";
+    document.getElementById("destino-entrega").textContent = `${fila[0].cidade}, ${fila[0].pais}`;
+
+    tempoEntrega = (Math.floor(Math.random() * 10) + 1) * 1000;
+    console.log(tempoEntrega)
+
+    const tempoTexto = document.getElementById("tempo-entrega");
+    let segundosRestantes = Math.floor(tempoEntrega / 1000);
+    tempoTexto.textContent = `⏳ ${segundosRestantes}s`;
+
+    const intervaloTempo = setInterval(() => {
+        segundosRestantes--;
+        if (segundosRestantes > 0) {
+            tempoTexto.textContent = `⏳ ${segundosRestantes}s`;
+        } else {
+            clearInterval(intervaloTempo);
+            tempoTexto.textContent = "✅ Pedido entregue";
+        }
+    }, 1000);
+    await new Promise(resolve => {
+        setTimeout(resolve, tempoEntrega)      
+        progrideBarra(tempoEntrega)
+    })
+
+    // Ocultar as informações de entrega após a conclusão
+    infoEntrega.style.display = "none"; // Ocultar as informações após a entrega
+}
+
+function progrideBarra(tempoEntrega) {
+    const progresso = document.getElementById('barra-progresso')
+    const porcentagem = document.getElementById('porcentagem-progresso')
+    let intervalo = setInterval(function() {
+        progresso.value = progresso.value + 1;
+        porcentagem.innerHTML = `${progresso.value}%`
+    
+        if (progresso.value == 100) {
+          clearInterval(intervalo);
+          progresso.value = 0
+          porcentagem.innerHTML = '0%'
+        }
+      }, tempoEntrega/100);
 }
 
 // Atualiza visualmente a lista da fila
